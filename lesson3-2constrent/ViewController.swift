ba5889c3e06b3e328fb0c6304e0d42008daf677f
пгпг <>
2023-02-21 00:13:11 +0600

    Initial Commit

---
+++ lesson3-2constrent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson3-2constrent
+//
+//  Created by cpu on 21/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
