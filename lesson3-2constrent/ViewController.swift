b06b0d644add2d14f333d7235f7e65d14d0af5cb
пгпг <>
2023-02-24 18:47:56 +0600

    Hw2

--- lesson3-2constrent/ViewController.swift
+++ lesson3-2constrent/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.setTitle("IOS", for: .normal)
     }
 
 
